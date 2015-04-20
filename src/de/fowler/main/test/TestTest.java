80f516079da33066b2e7ad49a6cc204f1728fefc
Oliver <oliver@fam-mahlke.de>
2015-04-20 10:17:42 +0200

    Added a Junit Test test

---
+++ src/de/fowler/main/test/TestTest.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package de.fowler.main.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.fowler.main.Program;
+
+/**
+ * @author root
+ *
+ */
+public class TestTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link de.fowler.main.Program#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
