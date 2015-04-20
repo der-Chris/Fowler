efb3d0b29ccad88b52a95713ab5d85b8fced619d
Oliver <oliver@fam-mahlke.de>
2015-04-20 10:13:39 +0200

    No Raw Types anymore

--- src/de/fowler/main/Customer.java
+++ src/de/fowler/main/Customer.java
@@ -5,7 +5,7 @@ import java.util.Vector;
 
 class Customer {
     private String name;
-    private Vector rentals = new Vector();
+    private Vector<Rental> rentals = new Vector<>();
     public Customer (String newname){
         name = newname;
     };
@@ -18,7 +18,7 @@ class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
+        Enumeration<Rental> enum_rentals = rentals.elements();	    
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
