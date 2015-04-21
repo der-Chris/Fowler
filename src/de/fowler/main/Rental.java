35936cec2824e594d0b987f36191ee24dabcdcbb
Oliver <oliver@fam-mahlke.de>
2015-04-21 14:11:33 +0200

    thisAmount -> price

--- src/de/fowler/main/Rental.java
+++ src/de/fowler/main/Rental.java
@@ -14,23 +14,23 @@ public class Rental {
     }
     
     public double getPrice() {
-    	 double thisAmount = 0;
+    	 double price = 0;
          switch (this.getMovie().getPriceCode()) {
              case REGULAR:
-                 thisAmount += 2;
+                 price += 2;
                  if (this.getDaysRented() > 2)
-                     thisAmount += (this.getDaysRented() - 2) * 1.5;
+                     price += (this.getDaysRented() - 2) * 1.5;
                  break;
              case NEW:
-                 thisAmount += this.getDaysRented() * 3;
+                 price += this.getDaysRented() * 3;
                  break;
              case CHILDREN:
-                 thisAmount += 1.5;
+                 price += 1.5;
                  if (this.getDaysRented() > 3)
-                     thisAmount += (this.getDaysRented() - 3) * 1.5;
+                     price += (this.getDaysRented() - 3) * 1.5;
                  break;
          }
-         return thisAmount;
+         return price;
     }
     
     public int getRenterPoints() {
